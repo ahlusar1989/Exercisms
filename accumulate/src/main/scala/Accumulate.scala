b1eb01a45e0ebb4c64f6002d614d291c0ca2b3ac
Saran Ahluwalia <ahlusar.ahluwalia@gmail.com>
2017-05-01 13:47:46 -0400

    alternate without map

--- accumulate/src/main/scala/Accumulate.scala
+++ accumulate/src/main/scala/Accumulate.scala
@@ -2,7 +2,7 @@
   * Created by saranahluwalia on 4/16/17.
   */
 class Accumulate {
-  def accumulate[T, U](op: (T) => U, input: Seq[T]): Seq[U] = {
-    input.map(x => op(x))
+  def accumulate[T, U](op: (T) => U, input: List[T]): Seq[U] = {
+    input.foldRight(Seq.empty[U])(((element, input ) => op(element) +: input))
   }
 }
